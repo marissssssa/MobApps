1f74bfcebdeb0143112a493e809c03ef69fbac76
518027 <rutt3644@gmail.com>
2025-06-01 10:27:36 +0700

    Create app_localizations_id.dart

---
+++ lib/l10n/app_localizations_id.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Indonesian (`id`).
+class AppLocalizationsId extends AppLocalizations {
+  AppLocalizationsId([String locale = 'id']) : super(locale);
+
+  @override
+  String get settings => 'Pengaturan';
+
+  @override
+  String get myProfile => 'Profil Saya';
+
+  @override
+  String get language => 'Bahasa';
+
+  @override
+  String get accessibilityMode => 'Mode Aksesibilitas';
+
+  @override
+  String get notifications => 'Notifikasi';
+
+  @override
+  String get logout => 'Keluar';
+}
