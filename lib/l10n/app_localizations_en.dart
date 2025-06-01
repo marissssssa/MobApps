c6790d55027977c521b36002d2d53a1e408aaa22
518027 <rutt3644@gmail.com>
2025-06-01 10:27:06 +0700

    Create app_localizations_en.dart

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,28 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get myProfile => 'My Profile';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get accessibilityMode => 'Accessibility Mode';
+
+  @override
+  String get notifications => 'Notifications';
+
+  @override
+  String get logout => 'Logout';
+}
